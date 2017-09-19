d8e93e42f94561a0aad60cd7092bc4b05e413302
robmckinnon <rob@movingflow>
2017-09-19 08:49:14 +0100

    Remove JSON stringify use, to fix e2e test startup

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,12 @@
+const stringify = (val) => {
+  if (val) {
+    return `"${val}"`;
+  } else {
+    return 'null';
+  }
+};
+
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+  API_BASE_URL: stringify(process.env.API_BASE_URL),
 }
