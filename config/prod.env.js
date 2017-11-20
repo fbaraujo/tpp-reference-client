326b11a82ebaedc47e395dbca0cc30febdec5fd8
robmckinnon <rob@movingflow>
2017-11-20 16:11:25 +0000

    Fix accountRequestConsentUri to use host from API_BASE_URL
    Fix accountRequestConsentUri to contain correct host when deployed to heroku.

--- config/prod.env.js
+++ config/prod.env.js
@@ -9,6 +9,5 @@ const stringify = (val) => {
 module.exports = {
   NODE_ENV: '"production"',
   API_BASE_URL: stringify(process.env.API_BASE_URL),
-  ACCOUNT_REQUEST_CONSENT_URI: stringify(process.env.ACCOUNT_REQUEST_CONSENT_URI),
   REDIRECT_DELAY_SECONDS: process.env.REDIRECT_DELAY_SECONDS,
 };
