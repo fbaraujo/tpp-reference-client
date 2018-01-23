767d3e2a1c80cd6e3327df41fcd0590f6505e438
jenjon700 <>
2018-01-23 14:48:08 +0000

    Add main.css file
    Add style-loader and css-loader to allow import of separate css file.
    Change HTML for new styling.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
+import './main.css';
 import App from './App';
 import store from './store';
 import router from './router';
