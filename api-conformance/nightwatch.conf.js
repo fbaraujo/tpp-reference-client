a89a779f6be2dae1f3c300e7eb53c66bf18d62e1
Ezo Saleh <ezo@the-experimenters.com>
2018-02-07 12:50:44 +0000

    nightwatch config now in JSON for easier configuration.

--- api-conformance/nightwatch.conf.js
+++ api-conformance/nightwatch.conf.js
@@ -5,46 +5,13 @@ require('nightwatch-cucumber')({
   cucumberArgs: ['--format', 'node_modules/cucumber-pretty', 'api-conformance/features'],
 });
 
-// http://nightwatchjs.org/gettingstarted#settings-file
-module.exports = {
-  src_folders: ['api-conformance/features'],
-  output_folder: 'api-conformance/reports',
-  custom_assertions_path: ['api-conformance/custom-assertions'],
+module.exports = (function(settings) {
+  settings.test_workers = false;
 
-  selenium: {
-    start_process: true,
-    server_path: require('selenium-server').path, // eslint-disable-line
-    host: '127.0.0.1',
-    port: 4444,
-    cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path, // eslint-disable-line
-    },
-  },
+  settings.selenium.server_path = require('selenium-server').path; // eslint-disable-line
+  settings.selenium.cli_args['webdriver.chrome.driver'] = require('chromedriver').path; // eslint-disable-line
 
-  test_settings: {
-    default: {
-      selenium_port: 4444,
-      selenium_host: 'localhost',
-      silent: true,
-      globals: {
-        devServerURL: `http://localhost:${process.env.PORT || config.dev.port}`,
-      },
-    },
+  settings.test_settings.default.globals.devServerURL = `http://localhost:${process.env.PORT || config.dev.port}`;
 
-    chrome: {
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-      },
-    },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-      },
-    },
-  },
-};
+  return settings;
+})(require('./nightwatch.json'));
