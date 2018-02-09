a024f638fd0b6d363d76eab743215af4cd32b7f7
Ezo Saleh <ezo@the-experimenters.com>
2018-02-09 15:02:37 +0000

    Can generate features with single payloads based on csv config.

--- api-conformance/runner.js
+++ api-conformance/runner.js
@@ -1,15 +1,42 @@
 const csv = require('csvtojson');
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'testing';
 const server = require('../build/dev-server.js');
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+function assembleTemplate(literal, params) {
+    return new Function(params, "return `"+literal+"`;");
+}
+
+const generateFeature = async (csvConfig) => {
+  const payments = csvConfig.group[0];
+
+  const groupPath = path.join(__dirname, 'features', payments);
+  const templatePath = path.join(groupPath, `${payments}.feature.template`);
+  const template = assembleTemplate((await readFile(templatePath)).toString('utf-8'), "templatePayload");
+  const templatePayload = `${ csvConfig.payload.reduce((acc, curr) => `${acc}
+    | ${curr[0]}           | ${curr[1]}           |`, `  | field            | value             |`) }`;
+  const featureContent = template(templatePayload);
+  const featurePath = path.join(groupPath, `${payments}.feature`);
+  await writeFile(featurePath, featureContent);
+  return featurePath;
+};
+
 const parser = (path) => {
-  const result = { group: [] };
+  const result = { group: [], payload: [] };
   return new Promise((resolve, reject) => {
     csv()
     .fromFile(path)
     .on('json', (obj) => {
       result.group.push(obj.conformance_type);
+      const keys = Object.keys(obj);
+      keys.shift();
+      result.payload = keys.map(k => [ k, obj[k]]);
     })
     .on('done', (error) => {
       if (error){
@@ -45,6 +72,7 @@ const startRunner = async () => {
 
   const csvConfig = await extractCsvConfigIfAny(opts);
   if (csvConfig) {
+    featurePath = await generateFeature(csvConfig);
     opts = opts.concat(['--group', csvConfig.group.join(',')]);
   }
 
